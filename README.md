27ced1094b035c12d2fd7fcbc9aa5be9093fc506
George <george.koshelev@hackerdom.ru>
2012-11-07 23:22:24 +0500

    show total memory for all efi memory types , no more ranges for each item in map.

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ OS
 3-е задание: Память EFI.
 
 Сама виртуальная машина запускалась под windows , необходимы: Qemu-windows-1.2.0 , OVMF_IA32
-запуск quemu qemu-system-i386w.exe -L Bios -hda fat:hda-contents
-
+Пример .bat файла для запуска VM:
+	quemu qemu-system-i386w.exe -L Bios -hda fat:hda-contents
 Efi файл компилился под Unutu 10.04. makefile в комплекте.
+
+Результат выполнения программы - список Efi memory type-ов и соответствующие им значения памяти.
--- UEFI_Memory/memory.c
+++ UEFI_Memory/memory.c
@@ -10,8 +10,6 @@ EFI_STATUS PrintMemoryMap(void);
 EFI_STATUS GetMemoryMap(EFI_MEMORY_DESCRIPTOR ** map , UINTN * size , UINTN * key , UINTN * dsize , UINT32 * dversion );
 void HandleAllocatePoolError(EFI_STATUS err);
 void HandleGetMemoryMapError(EFI_STATUS err);
-void PrintMemoryStatus(UINTN size , UINTN key , UINTN dsize , UINT32 dversion);
-void PrintMappingStatus(int index , EFI_MEMORY_DESCRIPTOR *pointer );
 
 const string DisplayMemoryTypes[] = {
     L"EfiReservedMemoryType",
@@ -41,15 +39,6 @@ const string GetMemoryTypeDisplayValue(UINT32 MemoryType){
 		
 }
 
-void PrintMappingStatus(int index , EFI_MEMORY_DESCRIPTOR *pointer){
-	UINTN size = pointer->NumberOfPages * 4096; 
-	EFI_PHYSICAL_ADDRESS PhysicalEnd = pointer->PhysicalStart + size;
-	EFI_VIRTUAL_ADDRESS VirtualEnd = pointer->VirtualStart + size;
-	Print(L"%d Type: %s\n", index , GetMemoryTypeDisplayValue(pointer->Type));
-	Print(L"PhysicalMemory : %016llx-%016llx\n" , pointer->PhysicalStart , PhysicalEnd);
-	Print(L"VirtualMemory : %016llx-%016llx\n\n", pointer->VirtualStart , VirtualEnd);
-}
-
 EFI_STATUS PrintMemoryMap(void){
 	EFI_STATUS err;
 	EFI_MEMORY_DESCRIPTOR * map;
@@ -57,18 +46,72 @@ EFI_STATUS PrintMemoryMap(void){
 	UINTN key;
 	UINTN dsize;
 	UINT32 dversion;
+	UINT64 MemoryArray[14] = {0};
+	UINT64 TotalMemory = 0;
 	err = GetMemoryMap (&map , &size , &key , &dsize , &dversion);
 	if (err != EFI_SUCCESS)
 		return err;
-	PrintMemoryStatus(size,key,dsize,dversion);
 	EFI_MEMORY_DESCRIPTOR * pointer = map;
 	int i = 0;
-
+	UINT64 temp;
 	while((void *) pointer < (void *) map + size){
-		PrintMappingStatus(i , pointer);
+		temp = pointer->NumberOfPages*4096;
+		switch(pointer->Type){
+			case EfiReservedMemoryType:
+				MemoryArray[0] += temp;
+				break;
+			case EfiLoaderCode:
+				MemoryArray[1] +=temp;
+				break;
+			case EfiLoaderData:
+				MemoryArray[2] += temp;
+				break;
+			case EfiBootServicesCode:
+				MemoryArray[3] += temp;
+				break;
+			case EfiBootServicesData:
+				MemoryArray[4] += temp;
+				break;
+			case EfiRuntimeServicesCode:
+				MemoryArray[5] += temp;
+				break;
+			case EfiRuntimeServicesData:
+				MemoryArray[6] += temp;
+				break;
+			case EfiConventionalMemory:
+				MemoryArray[7] += temp;
+				break;
+			case EfiUnusableMemory:
+				MemoryArray[8] += temp;
+				break;
+			case EfiACPIReclaimMemory:
+				MemoryArray[9] += temp;
+				break;
+			case EfiACPIMemoryNVS:
+				MemoryArray[10] += temp;
+				break;
+			case EfiMemoryMappedIO:
+				MemoryArray[11] += temp;
+				break;
+			case EfiMemoryMappedIOPortSpace:
+				MemoryArray[12] += temp;
+				break;
+			case EfiPalCode:
+				MemoryArray[13] += temp;
+				break;
+		}
+		TotalMemory += temp;	
 		pointer = (void *) pointer + dsize;
 		++i;
 	}
+	
+	for(i = 0 ; i < 14 ; i++){
+		Print(L"%s : %d\n" , DisplayMemoryTypes[i] , MemoryArray[i] );
+	}
+	Print(L"Total Memory : %d\n" , TotalMemory );
+
+	uefi_call_wrapper(BS->FreePool , 1 , map);
+
 	return EFI_SUCCESS;
 }
 
@@ -84,7 +127,6 @@ EFI_STATUS GetMemoryMap(EFI_MEMORY_DESCRIPTOR ** map , UINTN * size , UINTN * ke
 		}
 		err = uefi_call_wrapper(BS->GetMemoryMap , 5 , size , *map , key,dsize,dversion);
 		if (err != EFI_SUCCESS){
-			HandleGetMemoryMapError(err);
 			if (err != EFI_BUFFER_TOO_SMALL ) return err;
 			uefi_call_wrapper(BS->FreePool, 1, (void *)*map);	
 		}
@@ -97,27 +139,21 @@ EFI_STATUS GetMemoryMap(EFI_MEMORY_DESCRIPTOR ** map , UINTN * size , UINTN * ke
 void HandleAllocatePoolError(EFI_STATUS err){
 	Print(L"Allocation error: ");
 	if (err == EFI_OUT_OF_RESOURCES){
-		Print(L"The pool requested could not be allocated.");
+		Print(L"The pool requested could not be allocated.\n");
 	}
 	else if (err == EFI_INVALID_PARAMETER){
-		Print(L"PoolType was invalid.");
+		Print(L"PoolType was invalid.\n");
 	}
 	else{
-		Print(L"Buffer was NULL.");
+		Print(L"Buffer was NULL.\n");
 	}
 }
 void HandleGetMemoryMapError(EFI_STATUS err){
 	Print(L"Get Memory Map error: ");
 	if (err == EFI_BUFFER_TOO_SMALL){
-		Print(L"The MemoryMap buffer was too small.");
+		Print(L"The MemoryMap buffer was too small.\n");
 	}
-	Print(L"MemoryMapSize is NULL or The MemoryMap buffer is not too small and MemoryMap is NULL."
-	);
-}
-void PrintMemoryStatus(UINTN size , UINTN key , UINTN dsize , UINT32 dversion){
-	Print(L"MemoryMapSize: %d\n",size);
-	Print(L"MapKey: %d\n",key);
-	Print(L"DescriptorSize: %d\n",dsize);
-	Print(L"DescriptorVersion : %d\n",dversion);
-	Print(L"-------------------------------------------------------------\n");
+	else{
+	Print(L"MemoryMapSize is NULL or The MemoryMap buffer is not too small and MemoryMap is NULL.");
+	}
 }
